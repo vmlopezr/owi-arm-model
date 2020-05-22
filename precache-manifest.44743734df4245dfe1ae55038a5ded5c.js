850e23b07e90571853212bcd2e38d79b6ef5063b
Victor Lopez <vmlopez.r@gmail.com>
2020-05-22 02:23:34 -0500

    Updates

---
+++ precache-manifest.44743734df4245dfe1ae55038a5ded5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94d95cdf05d76595d01e328b9f2f29d",
+    "url": "/owi-arm-model/index.html"
+  },
+  {
+    "revision": "b5a3b4629f42e76360c7",
+    "url": "/owi-arm-model/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d9f050e57ccbe60ad8ce",
+    "url": "/owi-arm-model/static/css/main.22f0d04f.chunk.css"
+  },
+  {
+    "revision": "b5a3b4629f42e76360c7",
+    "url": "/owi-arm-model/static/js/2.5efba10b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/owi-arm-model/static/js/2.5efba10b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f050e57ccbe60ad8ce",
+    "url": "/owi-arm-model/static/js/main.dc8eaf79.chunk.js"
+  },
+  {
+    "revision": "c6df9650830192e87e97",
+    "url": "/owi-arm-model/static/js/runtime-main.5182d4d9.js"
+  }
+]);
