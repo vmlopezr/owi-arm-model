b5e6724b43d07fd9f3c8fabedbe601c2afec2fdd
Victor Lopez <vmlopez.r@gmail.com>
2020-05-22 01:59:10 -0500

    Adding static files for pages.

---
+++ docs/precache-manifest.5ce3d38912ecf078dd423fa03fb44b3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b426408a06681dafcdb2a6893e1832fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5a3b4629f42e76360c7",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d9f050e57ccbe60ad8ce",
+    "url": "/static/css/main.22f0d04f.chunk.css"
+  },
+  {
+    "revision": "b5a3b4629f42e76360c7",
+    "url": "/static/js/2.5efba10b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5efba10b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f050e57ccbe60ad8ce",
+    "url": "/static/js/main.dc8eaf79.chunk.js"
+  },
+  {
+    "revision": "3a7f689ae8f581d1dd9e",
+    "url": "/static/js/runtime-main.84192c91.js"
+  }
+]);
